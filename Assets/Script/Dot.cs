aecf4c2ccce747a4da002efe221b81ec925042f3
박성민 <moktak072@gmail.com>
2019-02-19 13:22:48 +0900

    2019 02 19
    SmoothDamp를 사용하지 않고 구현하기 전 백업

--- Assets/Script/Dot.cs
+++ Assets/Script/Dot.cs
@@ -4,26 +4,12 @@ using UnityEngine;
 
 public class Dot : MonoBehaviour {
 
-	private Vector2 firstTouchPosition;
-	private Vector2 finalTouchPosition;
-	private GameObject otherDot;
-	private GameObject tempDot;
-	private Board board;
-	private Vector2 targetPosition;
 
-	public float swipeAngle = 0;
+	private Board board;
+
 	public int column;
 	public int row;
-	public float swipeResist = 1f;
 
-	public bool swipeing = false;
-	public bool dropping = false;
-
-	public int nullUnderMe = 0;
-
-	private Vector2 velocity = Vector2.zero;
-
-	public bool isMatched = false;
 
 	void Start () {
 		board = FindObjectOfType<Board> ();
--- Assets/Script/DotManager.cs
+++ Assets/Script/DotManager.cs
@@ -6,23 +6,31 @@ public class DotManager : MonoBehaviour {
 
     private Board board;
 
-    GameObject selectedDot;
-    GameObject neighborDot;
+    GameObject selectedDot = null;
+    GameObject neighborDot = null;
+    GameObject _selectedDot = null;
+    GameObject _neighborDot = null;
+
+    Vector2 selectedVelocity = Vector2.zero;
+    Vector2 neighborVelocity = Vector2.zero;
 
     // 각도계산
-    private Vector2 firstTouchPosition;
-    private Vector2 finalTouchPosition;
+    private Vector2 firstTouchPosition = Vector2.zero;
+    private Vector2 finalTouchPosition = Vector2.zero;
     private float swipeAngle = 0f;
 
-	void Start () {
-        board = FindObjectOfType<Board>();
-	}
-	
+    private bool movingState = false;
 
-   	void Update () {
+    void Start() {
+        board = FindObjectOfType<Board>();
+        StartCoroutine(ChangeDotPositionCo());
+    }
+
+
+    void Update() {
 
         // raycast로 마우스가 있는 위치의 object를 가져온다
-		if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Ray2D ray = new Ray2D(firstTouchPosition, Vector2.zero);
@@ -57,6 +65,7 @@ public class DotManager : MonoBehaviour {
             MoveDots(selectedDot, neighborDot);
 
         }
+
     }
 
     private void SwipeDots()
@@ -64,8 +73,9 @@ public class DotManager : MonoBehaviour {
         int row = selectedDot.GetComponent<Dot>().row;
         int column = selectedDot.GetComponent<Dot>().column;
 
+        Debug.Log("스와이프 시작");
         // Swipe With Up
-        if(swipeAngle > 60 && swipeAngle < 120)
+        if (swipeAngle > 60 && swipeAngle < 120)
         {
             // neighborDot 지정
             neighborDot = board.allDots[column, row + 1];
@@ -75,7 +85,7 @@ public class DotManager : MonoBehaviour {
             selectedDot.GetComponent<Dot>().row++;
         }
         // Swipe with Down
-        else if (swipeAngle < -60 && swipeAngle > -120) 
+        else if (swipeAngle < -60 && swipeAngle > -120)
         {
             // neighborDot 지정
             neighborDot = board.allDots[column, row - 1];
@@ -83,12 +93,12 @@ public class DotManager : MonoBehaviour {
             // [column, row] 이동
             neighborDot.GetComponent<Dot>().row++;
             selectedDot.GetComponent<Dot>().row--;
-        } 
+        }
         // Swipe with Right
-        else if (swipeAngle > - 30 && swipeAngle < 30)
+        else if (swipeAngle > -30 && swipeAngle < 30)
         {
             // neighborDot 지정
-            neighborDot = board.allDots[column + 1,  row];
+            neighborDot = board.allDots[column + 1, row];
 
             // [column, row] 이동
             neighborDot.GetComponent<Dot>().column--;
@@ -109,31 +119,57 @@ public class DotManager : MonoBehaviour {
         {
             selectedDot = null;
             firstTouchPosition = Vector2.zero;
-            return ;
+            return;
         }
 
+        _selectedDot = selectedDot;
+        _neighborDot = neighborDot;
 
-        return ;
+
+        movingState = true;
+        return;
     }
 
     // board 의 allDots[]에 column, row 가 바뀌어 있는 상태에서 좌표를 목적지로 하여 Dot 이미지를 이동시키는 메서드
     private void MoveDots(GameObject _selectedDot, GameObject _neighborDot)
     {
-        Vector2 selectedVelocity = Vector2.zero;
-        Vector2 neighborVelocity = Vector2.zero;
+
+
+        Debug.Log("움직이기 시작");
 
         // Dot 이동
+        {
+            Debug.Log("Loop");
+
+
+            Debug.Log("Break");
+
+            //if (Mathf.Abs(_selectedDot.transform.position.x - _selectedDot.GetComponent<Dot>().column) < 0.07f)
+
+        }
+    }
+
+    private IEnumerator ChangeDotPositionCo()
+    {
         while (true)
         {
-            _selectedDot.transform.position = Vector2.SmoothDamp(_selectedDot.transform.position, new Vector2(_selectedDot.GetComponent<Dot>().column, _selectedDot.GetComponent<Dot>().row), ref selectedVelocity, 1f, 10f, Time.deltaTime);
-            _neighborDot.transform.position = Vector2.SmoothDamp(_neighborDot.transform.position, new Vector2(_neighborDot.GetComponent<Dot>().column, _neighborDot.GetComponent<Dot>().row), ref selectedVelocity, 1f, 10f, Time.deltaTime);
-        
-            if(Mathf.Abs(_selectedDot.transform.position.x - _selectedDot.GetComponent<Dot>().column) < 0.07f)
+            if (movingState)
             {
-                _selectedDot.transform.position = new Vector2(_selectedDot.GetComponent<Dot>().column, _selectedDot.GetComponent<Dot>().row);
-                _neighborDot.transform.position = new Vector2(_neighborDot.GetComponent<Dot>().column, _neighborDot.GetComponent<Dot>().row);
-                break;
+                _selectedDot.transform.position = Vector2.SmoothDamp(_selectedDot.transform.position, new Vector2(_selectedDot.GetComponent<Dot>().column, _selectedDot.GetComponent<Dot>().row), ref selectedVelocity, 1f, 100f, Time.deltaTime);
+                _neighborDot.transform.position = Vector2.SmoothDamp(_neighborDot.transform.position, new Vector2(_neighborDot.GetComponent<Dot>().column, _neighborDot.GetComponent<Dot>().row), ref selectedVelocity, 1f, 100f, Time.deltaTime);
+
+                if (((Mathf.Abs(_selectedDot.transform.position.x - _selectedDot.GetComponent<Dot>().column) < 0.07f) && (Mathf.Abs(_selectedDot.transform.position.y - _selectedDot.GetComponent<Dot>().row)) < 0.07f))
+                {
+                    _selectedDot.transform.position = new Vector2(_selectedDot.GetComponent<Dot>().column, _selectedDot.GetComponent<Dot>().row);
+                    _neighborDot.transform.position = new Vector2(_neighborDot.GetComponent<Dot>().column, _neighborDot.GetComponent<Dot>().row);
+                    movingState = false;
+
+                    yield return null;
+
+                }
             }
+
+            yield return null;
         }
     }
 }
